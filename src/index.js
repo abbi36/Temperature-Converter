764009a460e6ad63e28c6d4f4f846436382739b7
Abbi Engel <abbi.engel33@gmail.com>
2022-05-04 12:34:30 -0500

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let celsius = document.getElementById('celsius');
 let fahrenheit = document.getElementById('fahrenheit');
+let kelvin = document.getElementById('kelvin');
+let k = document.getElementById('kelvin');
 
 celsius.oninput = () => {
     let output = (parseFloat(celsius.value) * 9) /5 +32;
@@ -10,3 +12,13 @@ fahrenheit.oninput = () => {
     let output = ((parseFloat(fahrenheit.value) - 32) * 5) /9;
     celsius.value = parseFloat(output.toFixed(2));
 };
+
+kelvin.oninput = () => {
+    let output = ((parseFloat(kelvin.value) -273.15) *1.8) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+kelvin.oninput = () => {
+    let output = ((parseFloat(kelvin.value) -273.15));
+    celsius.value = parseFloat(output.toFixed(2));
+}
